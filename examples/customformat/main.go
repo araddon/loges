7ffde4202bc25f466787cf4226c79629ef351b09
ubuntu <araddon@gmail.com>
2013-06-07 15:30:12 -0700

    update example

--- examples/customformat/main.go
+++ examples/customformat/main.go
@@ -3,59 +3,74 @@ package main
 import (
 	"flag"
 	"github.com/araddon/loges"
-	"math"
+	"os"
+	"strings"
+	"time"
 )
 
 var (
-	msgChan      = make(chan *loges.LineEvent, 1000)
-	hostname     string
-	logConfig    string
-	logLevel     string
-	logType      string
-	esIndex      string
-	format       string
-	kafkaHost    string
-	topic        string
-	partitionstr string
-	offset       uint64
-	maxSize      uint
-	maxMsgCt     uint64
+	msgChan     = make(chan *loges.LineEvent, 1000)
+	hostname    string
+	logType     string
+	loc                = time.UTC
+	currentHost string = "Unknown"
 )
 
 func init() {
-	flag.StringVar(&hostname, "hostname", "lio26", "host:port string for the kafka server")
-	flag.StringVar(&logConfig, "logconfig", "timber.xml", "file for logging config")
-	flag.StringVar(&logLevel, "loglevel", "INFO", "loglevel [NONE,DEBUG,INFO,WARNING,ERROR]")
-	flag.StringVar(&format, "source", "fluentd", "Format [fluentd,kafka]")
+	flag.StringVar(&hostname, "eshost", "localhost:9200", "host string for the Elasticsearch Server")
 	flag.StringVar(&logType, "logtype", "datatype", "Type of data for elasticsearch index")
-	// kafka config info
-	flag.StringVar(&kafkaHost, "hostname", "localhost:9092", "host:port string for the kafka server")
-	flag.StringVar(&topic, "topic", "test", "topic to publish to")
-	flag.StringVar(&partitionstr, "partitions", "0", "partitions to publish to:  comma delimited")
-	flag.Uint64Var(&offset, "offset", 0, "offset to start consuming from")
-	flag.UintVar(&maxSize, "maxsize", 1048576, "max size in bytes to consume a message set")
-	flag.Uint64Var(&maxMsgCt, "msgct", math.MaxUint64, "max number of messages to read")
+	if host, err := os.Hostname(); err == nil {
+		currentHost = host
+	}
 }
 
 func main() {
 	flag.Parse()
-	loges.TimberSetLogging("[%D %T] %s %L %M", logLevel)
-	// update the index occasionally
+
+	// 1.  Start Output
+	// update the logstash index occasionally
 	go loges.UpdateLogstashIndex()
 	// start an elasticsearch writer worker, for sending to elasticsearch
-	go loges.ToElasticSearch(msgChan, "golog", hostname)
-	// custom formatter
-	loges.FormatterSet(loges.CustomFormatter(200))
-	go loges.StdinPruducer(msgChan)
+	go loges.ToElasticSearch(msgChan, "golog", cleanEsHost(hostname))
 
-	done := make(chan byte)
-	<-done
+	// 2.  Format/Filter
+	// create our custom formatter for parsing/filtering,/manipulating line entries
+	loges.FormatterSet(CustomFormatter(200))
+
+	// 3.  Input:  Start our Input and block
+	loges.StdinPruducer(msgChan)
 }
 
-// custom formatter
 func CustomFormatter(every uint64) loges.LineFormatter {
-	return func(e *loges.LineEvent) *loges.Event {
+	return func(d *loges.LineEvent) *loges.Event {
+		line := string(d.Data)
+		if strings.Contains(line, "DELETEME") {
+			return nil
+		}
+		parts := strings.SplitN(line, " ", 4)
+		if len(parts) < 2 {
+			return nil
+		}
 		// custom formatting
+		if _, err := time.Parse("2006/01/02 15:04:05.000000", parts[0]); err == nil {
+			evt := loges.NewTsEvent("mytype", d.Source, parts[2], time.Now().In(loc))
+			evt.Fields = make(map[string]interface{})
+			evt.Fields["host"] = currentHost
+			return evt
+		}
+		// if we return nil, it is filtered out
 		return nil
 	}
+}
+
+func cleanEsHost(oldHost string) string {
+	// It is possible that they sent a list of posts with :9200
+	hosts := strings.Split(oldHost, ",")
+	if len(hosts) > 0 {
+		parts := strings.Split(hosts[0], ":")
+		if len(parts) > 0 {
+			return parts[0]
+		}
+	}
+	return oldHost
 }
